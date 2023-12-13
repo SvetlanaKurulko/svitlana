f4ebfb84f75700d279bd8b55f98b59935f9c694a
ПК <svetochka116236@gmail.com>
2023-12-13 20:14:36 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# print("Nello world")
+# number = int(input("Enter 3-digit number: "))
+# n1 = number // 100
+# n2 = number // 10 % 10
+# n3 = number % 10
+# result = n1 + n2 + n3
+# print (f"n1: {n1} n2: {n2} n3 {n3}")
+# print (f"Result: {result}")
+#
+# number = int(input("Enter 3-digit number: "))
+# n1 = number // 100
+# n2 = number // 10 % 10
+# n3 = number % 10
+# result = n1 * n2 * n3
+# print (f"n1: {n1} n2: {n2} n3 {n3}")
+# print (f"Result: {result}")
+
+
+# number = int(input("Enter first digit diagonal: "))
+# n1 = number *1
+# number = int(input("Enter second digit diagonal: "))
+# n2 = number *1
+# result = (n1 * n2)/2
+# print (f"Result: {result}")
+
+
+# number = int(input("Enter 4-digit number: "))
+# n1 = number // 1000
+# n2 = number // 100 % 10
+# n3 = number // 10 % 10
+# n4 = number % 10
+# result = n1 * n2 * n3 * n4
+# print (f"n1: {n1} n2: {n2} n3 {n3} n4: {n4}")
+# print (f"Result: {result}")
